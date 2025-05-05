fc670ccaac6d530ed89f0781c364b5a9436c29bc
Nikita Nazarov <nvnazarov.me@gmail.com>
2025-05-06 01:57:40 +0300

    feat(datacat-ui): MVP notifications done

--- frontend/datacat-ui/src/features/alerting/edit-notification-group/edit-notification-group-form.component.ts
+++ frontend/datacat-ui/src/features/alerting/edit-notification-group/edit-notification-group-form.component.ts
@@ -7,35 +7,58 @@ import {
 } from '@angular/forms';
 import { from, timer } from 'rxjs';
 import {
-  FAKE_NOTIFICATION_CHANNEL,
   FAKE_NOTIFICATION_GROUP,
   getFakeNotificationChannel,
 } from '../../../shared/mock/fakes';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { NotificationChannel } from '../../../entities';
+import { NotificationChannelDriver } from '../../../entities';
+import { SelectModule } from 'primeng/select';
+import { InputGroupModule } from 'primeng/inputgroup';
 
 @Component({
   standalone: true,
   selector: 'datacat-edit-notification-group-form',
   templateUrl: './edit-notification-group-form.component.html',
   styleUrl: './edit-notification-group-form.component.scss',
-  imports: [ReactiveFormsModule, InputTextModule, ButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    SelectModule,
+    InputGroupModule,
+  ],
 })
 export class EditNotificationGroupFormComponent {
   @Input() public set groupId(id: string) {
     this.loadEssentials(id);
   }
 
+  protected drivers = Object.values(NotificationChannelDriver);
+
   protected isSavingInitiated = false;
+
+  protected driverControl = new FormControl<NotificationChannelDriver>(
+    NotificationChannelDriver.EMAIL,
+  );
 
   protected form = new FormGroup({
     name: new FormControl<string>(''),
-    channels: new FormArray<FormGroup>([]),
+    emailChannels: new FormArray<FormGroup>([]),
+    webhookChannels: new FormArray<FormGroup>([]),
+    telegramChannels: new FormArray<FormGroup>([]),
   });
 
-  protected get notificationChannels(): FormArray<FormGroup> {
-    return this.form.get('channels') as FormArray<FormGroup>;
+  protected get emailChannels(): FormArray<FormGroup> {
+    return this.form.get('emailChannels') as FormArray<FormGroup>;
+  }
+
+  protected get webhookChannels(): FormArray<FormGroup> {
+    return this.form.get('webhookChannels') as FormArray<FormGroup>;
+  }
+
+  protected get telegramChannels(): FormArray<FormGroup> {
+    return this.form.get('telegramChannels') as FormArray<FormGroup>;
   }
 
   protected loadEssentials(groupId: string) {
@@ -48,7 +71,11 @@ export class EditNotificationGroupFormComponent {
     from([
       [getFakeNotificationChannel(), getFakeNotificationChannel()],
     ]).subscribe({
-      next: () => {},
+      next: (notificationChannels) => {
+        notificationChannels.forEach((channel) => {
+          // TODO
+        })
+      },
       error: () => {},
     });
   }
@@ -70,8 +97,44 @@ export class EditNotificationGroupFormComponent {
   }
 
   protected addChannel() {
-    this.notificationChannels.push(new FormGroup({
-      address: new FormControl<string>(''),
-    }));
+    const driver = this.driverControl.value;
+    switch (driver) {
+      case NotificationChannelDriver.EMAIL: {
+        this.emailChannels.push(
+          new FormGroup({
+            address: new FormControl<string>(''),
+          }),
+        );
+        break;
+      }
+      case NotificationChannelDriver.TELEGRAM: {
+        this.telegramChannels.push(
+          new FormGroup({
+            username: new FormControl<string>(''),
+          }),
+        );
+        break;
+      }
+      case NotificationChannelDriver.WEBHOOK: {
+        this.webhookChannels.push(
+          new FormGroup({
+            url: new FormControl<string>(''),
+          }),
+        );
+        break;
+      }
+    }
+  }
+
+  protected deleteEmailChannel(index: number) {
+    this.emailChannels.removeAt(index);
+  }
+
+  protected deleteWebhookChannel(index: number) {
+    this.webhookChannels.removeAt(index);
+  }
+
+  protected deleteTelegramChannel(index: number) {
+    this.telegramChannels.removeAt(index);
   }
 }
