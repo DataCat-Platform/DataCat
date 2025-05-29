add60efcbc2404faaf773740ec0aa877d1a51d31
Nikita Nazarov <nvnazarov.me@gmail.com>
2025-05-29 15:33:53 +0300

    feat(datacat-ui): fix data sources styles

--- frontend/datacat-ui/src/shared/services/app-dialog.service.ts
+++ frontend/datacat-ui/src/shared/services/app-dialog.service.ts
@@ -1,30 +1,28 @@
-import {Injectable, Type} from '@angular/core';
-import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
+import { Injectable, Type } from '@angular/core';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppDialogService {
+  ref: DynamicDialogRef | undefined;
 
-    ref: DynamicDialogRef | undefined;
+  constructor(public dialogService: DialogService) {}
 
-    constructor(public dialogService: DialogService) {
-    }
-
-    showDialog(
-        component: Type<any>,
-        header: string,
-        data: any,
-        height: string = '1200px',
-        width: string = '800px',
-    ): void {
-        this.ref = this.dialogService.open(component, {
-            header: `${header}`,
-            data: data,
-            modal: true,
-            width: width!,
-            height: height!,
-            focusOnShow: false,
-        });
-    }
+  showDialog(
+    component: Type<any>,
+    header: string,
+    data: any,
+    height: string = '1200px',
+    width: string = '800px',
+  ): void {
+    this.ref = this.dialogService.open(component, {
+      header: `${header}`,
+      data: data,
+      modal: true,
+      focusOnShow: false,
+      closable: true,
+      draggable: true,
+    });
+  }
 }
